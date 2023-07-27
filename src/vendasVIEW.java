39dba810bd48bd2c1dd08319f7955101a4be6ea3
Lucas Melo <lusmelo.melo.silva@gmail.com>
2023-07-26 22:13:21 -0300

    tela-vendas

--- src/vendasVIEW.java
+++ src/vendasVIEW.java
@@ -21,7 +21,7 @@ public class vendasVIEW extends javax.swing.JFrame {
      */
     public vendasVIEW() {
         initComponents();
-        listaProdutos();
+
     }
 
     /**
@@ -147,26 +147,6 @@ public class vendasVIEW extends javax.swing.JFrame {
     // End of variables declaration//GEN-END:variables
 
 
-    private void listaProdutos(){
-        try {
-            ProdutosDAO produtosdao = new ProdutosDAO();
-            
-            DefaultTableModel model = (DefaultTableModel) listaProdutos.getModel();
-            model.setNumRows(0);
-            
-            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutosVendidos();
-            
-            for(int i = 0; i < listagem.size(); i++){
-                model.addRow(new Object[]{
-                    listagem.get(i).getId(),
-                    listagem.get(i).getNome(),
-                    listagem.get(i).getValor(),
-                    listagem.get(i).getStatus()
-                });
-            }
-        } catch (Exception e) {
-        }
-    
-    }
+
     
 }
